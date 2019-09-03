d92ba6fc6f8051003c1764257a0ccf92f844220e
Izdepskyi <Ragnarlobrok91>
2019-09-03 13:24:49 +0300

    5.Hibernate5-Save--persist--saveOrUpdate-example

--- src/main/java/com/infotech/client/ClientTest.java
+++ src/main/java/com/infotech/client/ClientTest.java
@@ -2,6 +2,7 @@ package com.infotech.client;
 
 import com.infotech.entities.Employee;
 import com.infotech.util.HibernateUtil;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import java.util.Date;
@@ -11,28 +12,45 @@ public class ClientTest {
     public static void main(String[] args) {
 
         try(Session session = HibernateUtil.getSessionFactory().openSession()){
-            Employee employee = getEmployee();
             session.beginTransaction ();
 
-            // session.persist (employee);
-            Integer id = (Integer)session.save (employee);
+            session.persist (getEmployee1 ());
+
+            Integer id = (Integer)session.save (getEmployee2 ());
             System.out.println ("Employee is created with id::" + id);
 
+            session.saveOrUpdate (getEmployee3 ());
 
             session.getTransaction ().commit ();
 
-        }catch(Exception e){
+        }catch(HibernateException e){
         e.printStackTrace();
         }
         }
 
-        private static Employee getEmployee() {
+        private static Employee getEmployee3() {
         Employee employee = new Employee ();
-        employee.setEmployeeName ("James Bond");
-        employee.setEmail ("email@gmail.com");
-        employee.setSalary (7000.00);
+        employee.setEmployeeName ("Martin Bingel");
+        employee.setEmail ("martin.email@gmail.com");
+        employee.setSalary (8000.00);
         employee.setDoj (new Date ());
         return employee;
         }
+    private static Employee getEmployee1() {
+        Employee employee = new Employee ();
+        employee.setEmployeeName ("Frank Bingel");
+        employee.setEmail ("frank.email@gmail.com");
+        employee.setSalary (3000.00);
+        employee.setDoj (new Date ());
+        return employee;
+    }
+    private static Employee getEmployee2() {
+        Employee employee = new Employee ();
+        employee.setEmployeeName ("Sean Bingel");
+        employee.setEmail ("sean.email@gmail.com");
+        employee.setSalary (6000.00);
+        employee.setDoj (new Date ());
+        return employee;
+    }
 
         }
